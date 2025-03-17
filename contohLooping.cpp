475936bf2a066690f501f07e1dd1a351389c999d
riswandaP <riswandaputra03@gmail.com>
2025-03-17 11:15:04 +0700

    Membuat perulangan FOR naik

--- contohLooping.cpp
+++ contohLooping.cpp
@@ -6,5 +6,16 @@ int main()
     int n;
     int perulanganWhile = 0;
     int perulanganDo = 0;
-    
+
+    // Perulangan dengan for
+
+    cout << "Perulangan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "Nilai n = " << n << " Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n << endl;
+    cout << endl;
+
+    cout << "Perulangan for pencacah turun" << endl;
 }
