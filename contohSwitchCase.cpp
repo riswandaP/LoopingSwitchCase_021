e08eaa6fb0c06957c0684f3625c0dbb52ab750bc
riswandaP <riswandaputra03@gmail.com>
2025-03-17 11:44:51 +0700

    Membuat tampilan MENU

--- contohSwitchCase.cpp
+++ contohSwitchCase.cpp
@@ -1,6 +1,17 @@
 int main()
 {
     int pilihan;
-    float panjang, lebar; 
+    float panjang, lebar;
     float jejari;
+
+    do
+    {
+        cout << "Menu" << endl;
+        cout << "1. Luas Persegi Panjang" << endl;
+        cout << "2. Luas Lingkaran" << endl;
+        cout << "3. Keluar" << endl;
+
+        cout << "Pilihan (1/2/3) = ";
+        cin >> pilihan;
+    }
 }
